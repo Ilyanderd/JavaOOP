40aa8fb5f597c7eab0d63b14f03925cf04235634
Ilyanderd <Povetkin_i@mail.ru>
2024-08-08 15:43:40 +0700

    Сделана задача Shapes часть 1

---
+++ Shapes/src/ru/academits/povetkin/shapes/Circle.java
@@ -0,0 +1,39 @@
+package ru.academits.povetkin.shapes;
+
+import static java.lang.Math.pow;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getWidth() {
+        return 2 * radius;
+    }
+
+    @Override
+    public double getHeight() {
+        return 2 * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * pow(radius,2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ Shapes/src/ru/academits/povetkin/shapes/Square.java
@@ -0,0 +1,39 @@
+package ru.academits.povetkin.shapes;
+
+import static java.lang.Math.pow;
+
+public class Square implements Shape {
+    private double sideLength;
+
+    public Square(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * sideLength;
+    }
+
+    @Override
+    public double getWidth() {
+        return sideLength;
+    }
+
+    @Override
+    public double getHeight() {
+        return sideLength;
+    }
+
+    @Override
+    public double getArea() {
+        return pow(sideLength, 2);
+    }
+}
